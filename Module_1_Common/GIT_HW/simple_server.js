6bb949ec1892c85129a1e4f9a2d8424fda4e0378
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2024-02-17 07:11:07 +0500

    init commit

---
+++ Module_1_Common/GIT_HW/simple_server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+
+const server = http.createServer(async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    try {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // Простой HTML-код
+        const htmlContent = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>simple_server</title>
+        </head>
+        <body>
+          <h1>HTML</h1>
+        </body>
+      </html>
+    `;
+
+        // Отправка HTML клиенту
+        res.end(htmlContent);
+    } catch (error) {
+        console.error('Ошибка:', error.message);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+    }
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
